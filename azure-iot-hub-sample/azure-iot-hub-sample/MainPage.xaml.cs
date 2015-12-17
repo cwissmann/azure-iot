6812d123684b7562ea07e0c4caa8bf2fae9e283e
Christian Wißmann <mail@c-wissmann.de>
2015-12-17 17:05:07 +0100

    add iot device functionality with fezhat

--- azure-iot-hub-sample/azure-iot-hub-sample/MainPage.xaml.cs
+++ azure-iot-hub-sample/azure-iot-hub-sample/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using GHIElectronics.UWP.Shields;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +27,59 @@ namespace azure_iot_hub_sample
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private FEZHAT hat;
+        private DispatcherTimer timer;
+
+        static DeviceClient deviceClient;
+        static string iotHubUri = "[iothuburi]";
+        static string deviceId = "[deviceid]";
+        static string deviceKey = "[devicekey]";
+        static string connectionString = "[connectionstring]";
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            try
+            {
+                deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Http1);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            this.Setup();
+        }
+
+        private async void Setup()
+        {
+            this.hat = await FEZHAT.CreateAsync();
+
+            this.timer = new DispatcherTimer();
+            this.timer.Interval = TimeSpan.FromSeconds(30);
+            this.timer.Tick += this.OnTick;
+            this.timer.Start();
+        }
+
+        private async void OnTick(object sender, object e)
+        {
+            var temperature = this.hat.GetTemperature().ToString("N2");
+            var lightLevel = this.hat.GetLightLevel().ToString("P2");
+
+            var weatherDataPoint = new
+            {
+                deviceId = deviceId,
+                temperature = temperature,
+                lightLevel = lightLevel,
+                time = DateTime.Now
+            };
+
+            var messageString = JsonConvert.SerializeObject(weatherDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+            await deviceClient?.SendEventAsync(message);
+
+            Debug.WriteLine("Message sent: " + message);
         }
     }
 }
